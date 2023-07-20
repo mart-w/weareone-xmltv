991250089abedff88cd38aa012ba49a9491e0c99
Martin Wurm <git@mart-w.de>
2023-07-20 02:43:08 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{pkgs ? import <nixpkgs> {}}:
+with pkgs;
+  rustPlatform.buildRustPackage {
+    pname = "wearone-xmltv";
+    version = "0.1.0";
+
+    src = ./.;
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+    };
+
+    meta = with lib; {
+      description = "An XMLTV provider for WeAreOne.FM internet radio stations.";
+      homepage = "https://codeberg.org/mart-w/weareone-xmltv";
+    };
+  }
